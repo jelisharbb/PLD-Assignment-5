2547839309133f29dc9754166a9b676a80ca0a75
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-14 22:11:39 +0800

    Printed a welcome remarks and defined a variable.

---
+++ Life-stages.py
@@ -0,0 +1,5 @@
+# print a welcome remark
+print ("\nGreetings! This program will determine if you are a kid, teen, debutant, or an adult based on the age that you will provide.")
+
+# ask input from the user
+userAge = int(input("\nPlease enter your age: "))
