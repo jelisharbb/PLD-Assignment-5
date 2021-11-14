35f191cb29bfe88b7f5902547515f66249ea73f0
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-14 15:27:53 +0800

    Created a welcome remarks, and defined a variable.

---
+++ PUP-Grading-System.py
@@ -0,0 +1,7 @@
+# greetings to the user
+print ("\nWelcome to PUP Grading System! \n")
+
+# asks for input
+userGrade = float(input("Please enter your grade percentage: "))
+roundedGrade = round(userGrade)
+
