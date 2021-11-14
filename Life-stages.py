12bc283e973b3e5e469f920fc901c29fc20dfc6e
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-14 22:21:08 +0800

    Added if and elif functions that can identify kids and teens.

--- Life-stages.py
+++ Life-stages.py
@@ -3,3 +3,9 @@ print ("\nGreetings! This program will determine if you are a kid, teen, debutan
 
 # ask input from the user
 userAge = int(input("\nPlease enter your age: "))
+
+# if else conditions
+if userAge >= 0 and userAge <= 12:
+    print ("Hi. You're a kid. \n")
+elif userAge >= 13 and userAge <= 17:
+    print ("Hi. You're a teen. \n")
