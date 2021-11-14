426119730584e8a675cf79bd8cadc2bf4eddb78d
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-14 22:53:44 +0800

    Added elif functions and expounded that adult stage.

--- Life-stages.py
+++ Life-stages.py
@@ -11,6 +11,12 @@ elif userAge >= 13 and userAge <= 17:
     print ("Hi! You're a teen. \n")
 elif userAge ==18:
     print ("Hi! You're a debutant. \n")
-else:
-    print ("Hi! You're an adult. \n")
+elif userAge >= 19 and userAge <= 45:
+    print ("Hi! You're an early adult. \n")
+elif userAge >= 46 and userAge <= 65:
+    print ("Hi! You're a middle adult. \n")
+elif userAge >= 66 and userAge <= 120:
+    print ("Hi! You're a late adult. \n")
+
+
 
