d5b449d9d39b387e5fe8fafcccc662fb8730cac2
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-15 16:59:01 +0800

    Changed the if else function.

--- Lowest-number.py
+++ Lowest-number.py
@@ -1,12 +1,10 @@
 # print welcome remarks
 print ("\nWelcome! This program can determine the lowest number among the three numbers that you would enter.")
 
-def lowestOfTheTwo (x, y, z):
-    lowest = x
-    print (f"The lowest number among the three is {x}.")
-    if y < lowest:
-        lowest = y
-        print (f"The lowest number among the three is {y}.")
+def lowestOfTheTwo(x, y, z):
+    if x < y and x < z:
+        print (f"\nThe lowest number among the three is {x}.\n")
+    elif y < x and y < z:
+        print (f"\nThe lowest number among the three is {y}.\n")
     else:
-        lowest = z
-        print (f"The lowest number among the three is {z}.")
+        print (f"\nThe lowest number among the three is {z}.\n")
