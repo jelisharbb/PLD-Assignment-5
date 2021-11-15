b510032b6336ce9ab25440574fa0fceb1c4eb779
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-15 17:03:30 +0800

    Defined a variables that ask inout from the user.

--- Lowest-number.py
+++ Lowest-number.py
@@ -1,6 +1,7 @@
 # print welcome remarks
 print ("\nWelcome! This program can determine the lowest number among the three numbers that you would enter.")
 
+# defined a function
 def lowestOfTheTwo(x, y, z):
     if x < y and x < z:
         print (f"\nThe lowest number among the three is {x}.\n")
@@ -8,3 +9,8 @@ def lowestOfTheTwo(x, y, z):
         print (f"\nThe lowest number among the three is {y}.\n")
     else:
         print (f"\nThe lowest number among the three is {z}.\n")
+
+# asks input from the user
+x_ = float(input("\nEnter a number: "))
+y_ = float(input("Enter another number: "))
+z_ = float(input("Enter another number: "))
