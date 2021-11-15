770b23d4442f69973bce20106f2dfdeac908847a
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-15 16:29:22 +0800

    Defined a function.

--- Lowest-number.py
+++ Lowest-number.py
@@ -1,3 +1,12 @@
 # print welcome remarks
 print ("\nWelcome! This program can determine the lowest number among the three numbers that you would enter.")
 
+def lowestOfTheTwo (x, y, z):
+    lowest = x
+    print (f"The lowest number among the three is {x}.")
+    if y < lowest:
+        lowest = y
+        print (f"The lowest number among the three is {y}.")
+    else:
+        lowest = z
+        print (f"The lowest number among the three is {z}.")
