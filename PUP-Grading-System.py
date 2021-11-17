5722cf4bea4e629da1750d184266b83364ad3f07
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-17 22:25:51 +0800

    Added an if function and stored the whole function into it. Adjusted also the indents.

--- PUP-Grading-System.py
+++ PUP-Grading-System.py
@@ -8,40 +8,32 @@ userGrade = input("\nPlease enter your grade percentage: ")
 if userGrade.isdigit() == True:
     roundedGrade = round(float(userGrade))
 
-# 1.0 to 1.75
-if roundedGrade >= 97 and roundedGrade <= 100:
-    print ("\nThis is the result based on your grade percentage \nGrade: 1.0 \nDescription: Excellent! \n")
-elif roundedGrade >= 94 and roundedGrade <= 96:
-    print ("\nThis is the result based on your grade percentage \nGrade: 1.25 \nDescription: Excellent! \n")
-elif roundedGrade >= 91 and roundedGrade <= 93:
-    print ("\nThis is the result based on your grade percentage \nGrade: 1.5 \nDescription: Very Good! \n")
-elif roundedGrade >= 88 and roundedGrade <= 90:
-    print ("\nThis is the result based on your grade percentage \nGrade: 1.75 \nDescription: Very Good! \n")
+    # 1.0 to 1.75
+    if roundedGrade >= 97 and roundedGrade <= 100:
+      print ("\nThis is the result based on your grade percentage \nGrade: 1.0 \nDescription: Excellent! \n")
+    elif roundedGrade >= 94 and roundedGrade <= 96:
+      print ("\nThis is the result based on your grade percentage \nGrade: 1.25 \nDescription: Excellent! \n")
+    elif roundedGrade >= 91 and roundedGrade <= 93:
+      print ("\nThis is the result based on your grade percentage \nGrade: 1.5 \nDescription: Very Good! \n")
+    elif roundedGrade >= 88 and roundedGrade <= 90:
+     print ("\nThis is the result based on your grade percentage \nGrade: 1.75 \nDescription: Very Good! \n")
 
-# 2.0 to 2.75
-elif roundedGrade >= 85 and roundedGrade <= 87:
-    print ("\nThis is the result based on your grade percentage \nGrade: 2.0 \nDescription: Good \n")
-elif roundedGrade >= 82 and roundedGrade <= 84:
-    print ("\nThis is the result based on your grade percentage \nGrade: 2.25 \nDescription: Good \n")
-elif roundedGrade >= 79 and roundedGrade <= 81:
-    print ("\nThis is the result based on your grade percentage \nGrade: 2.5 \nDescription: Satisfactory \n")
-elif roundedGrade >= 76 and roundedGrade <= 78:
-    print ("\nThis is the result based on your grade percentage \nGrade: 2.75 \nDescription: Satisfactory \n")
+    # 2.0 to 2.75
+    elif roundedGrade >= 85 and roundedGrade <= 87:
+      print ("\nThis is the result based on your grade percentage \nGrade: 2.0 \nDescription: Good \n")
+    elif roundedGrade >= 82 and roundedGrade <= 84:
+      print ("\nThis is the result based on your grade percentage \nGrade: 2.25 \nDescription: Good \n")
+    elif roundedGrade >= 79 and roundedGrade <= 81:
+      print ("\nThis is the result based on your grade percentage \nGrade: 2.5 \nDescription: Satisfactory \n")
+    elif roundedGrade >= 76 and roundedGrade <= 78:
+      print ("\nThis is the result based on your grade percentage \nGrade: 2.75 \nDescription: Satisfactory \n")
 
-# 3.0 and 5.0
-elif roundedGrade == 75:
-    print ("\nThis is the result based on your grade percentage \nGrade: 3.0 \nDescription: Passing \n")
-elif roundedGrade >= 65 and roundedGrade <= 74:
-    print ("\nThis is the result based on your grade percentage \nGrade: 5.0 \nDescription: Failure \n")
+    # 3.0 and 5.0
+    elif roundedGrade == 75:
+      print ("\nThis is the result based on your grade percentage \nGrade: 3.0 \nDescription: Passing \n")
+    elif roundedGrade >= 65 and roundedGrade <= 74:
+      print ("\nThis is the result based on your grade percentage \nGrade: 5.0 \nDescription: Failure \n")
  
-# if the user inputs number beyond the scope of the grading system
-elif roundedGrade > 100:
-    print ("\nOops! Seems like you entered an invalid number. Please enter a number within the scope of the PUP Grading System. \n")
-
-# incomplete
-elif roundedGrade < 65 and roundedGrade >= 0:
-     print ("\nThis is the result based on your grade percentage \nGrade: None \nDescription: Incomplete\Withdrawn\Dropped \n")
-
-# withdrawn 
-
-# dropped
+    # if the user inputs number beyond the scope of the grading system
+    elif roundedGrade > 100:
+      print ("\nOops! Seems like you entered an invalid number. Please enter a number within the scope of the PUP Grading System. \n")
