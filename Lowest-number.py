3807f0b65b12f0a2a9528a3c077111e95452a1ba
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-20 19:27:47 +0800

    Renamed the variables.

--- Lowest-number.py
+++ Lowest-number.py
@@ -2,13 +2,13 @@
 print ("\nWelcome! This program can determine the lowest number among the three numbers that you would enter.")
 
 # defined a function
-def lowestOfTheThree(x, y, z):
-    if x < y and x < z:
-        print (f"\nThe lowest number among the three is {x}.\n")
-    elif y < x and y < z:
-        print (f"\nThe lowest number among the three is {y}.\n")
+def lowestOfTheThree(first, second, third):
+    if first < second and first < third:
+        print (f"\nThe lowest number among the three is {first}.\n")
+    elif second < first and second < third:
+        print (f"\nThe lowest number among the three is {second}.\n")
     else:
-        print (f"\nThe lowest number among the three is {z}.\n")
+        print (f"\nThe lowest number among the three is {third}.\n")
 
 # asks input from the user
 firstNum = float(input("\nEnter a number: "))
